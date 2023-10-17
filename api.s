a325f69a2fb35742fa3c677038dd372e3b5ef143
Luan <luan.marko2@gmail.com>
2023-10-17 16:26:14 -0300

    [add] memory_alloc

--- api.s
+++ api.s
@@ -1,7 +1,7 @@
 .section .data
     # Isso tem que estar na BSS (Mudar depois)
     brk_original: .quad 0
-    brk_atual:    .quad 0
+    brk_current:    .quad 0
     
 .section .text
     # void setup_brk 
@@ -14,9 +14,9 @@
         movq $12, %rax
         syscall
 
-        # brk_original = brk_atual = BRK
+        # brk_original = brk_current = BRK
         movq %rax, brk_original
-        movq brk_original, brk_atual
+        movq brk_original, brk_current
 
         ret
 
@@ -25,8 +25,8 @@
         pushq %rbp
         movq %rsp, %rbp
 
-        # reseta  brk_atual
-        movq brk_original, brk_atual
+        # reseta  brk_current
+        movq brk_original, brk_current
 
         # Atualiza BRK
         movq brk_original, %rdi 
@@ -40,10 +40,16 @@
         movq %rsp, %rbp
 
         # Verifica se brk <= posição atual da heap
+        movq brk_original, %rbx
+        cmpq %rbx, brk_current
+        jle valid_position
             # Se sim, atualiza BRK = BRK + (Tamanho+16)
+            addq %rdi, brk_current
+            addq $16, brk_current
                 # Cria registro  
                 # FIM
             # Se não
+            valid_position:
                 # Verifica se o espaço esta livre
                     # Se sim, verifica tamanho
                         # Flag que está ocupado agora
